00fed66b3fb068e5a4edb0a9ebe3ba514f42b884
ZoondEngine <mango.kamchatka.1997@gmail.com>
2020-09-30 03:22:46 +0300

    Revert "Revert "[ADDOD]: Add log simple test plugin""
    This reverts commit a21bd7b1c619992a1b677924d89e7f4370316df8.

---
+++ Ex.Plugin.Log/LogExPlugin.cs
@@ -0,0 +1,43 @@
+﻿using Ex.Plug;
+
+using System;
+
+namespace Ex.Plugin.Log
+{
+    public class LogExPlugin : IExPlugin
+    {
+        public T As<T>() where T : IExPlugin
+        {
+            if ( typeof( T ) != GetType() )
+                return default;
+
+            return ( T )( object )this;
+        }
+
+        public string GetIdentifier()
+            => "Ex.Plugin.Log";
+
+        public string GetName()
+            => "ExLogger";
+
+        public string GetVersion()
+            => "1.0.0.0";
+
+        public void OnLoad()
+        {
+            Console.WriteLine($"Addon: {GetName()}(v.{GetVersion()}--{GetIdentifier()}) loaded!");
+        }
+
+        public void OnUnload()
+        {
+            Console.WriteLine( $"Addon: {GetName()}(v.{GetVersion()}--{GetIdentifier()}) unloaded!" );
+        }
+
+        public void ToConsole(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{DateTime.UtcNow}: {message}");
+            Console.ResetColor();
+        }
+    }
+}
